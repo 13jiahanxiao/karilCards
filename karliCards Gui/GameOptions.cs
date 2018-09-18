3b35b9b2bc815e14ced716f9d232cbbdf8c966c7
13jiahanxiao <2565659862@qq.com>
2018-09-18 21:47:43 +0800

    初级window开发

---
+++ karliCards Gui/GameOptions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace karliCards_Gui
+{
+    [Serializable]
+    public class GameOptions:INotifyPropertyChanged
+    {
+        private bool _playAgainstComputer = true;
+        private int _numberOfPlayers = 2;
+        private ComputerSkillLevel _computerSkill = ComputerSkillLevel.Dumb;
+        public bool PlayAgainstComputer
+        {
+            get { return _playAgainstComputer; }
+            set
+            { _playAgainstComputer = value;
+                OnPropertyChanged(nameof(PlayAgainstComputer));
+            }
+        }
+        public int NumberOfPlayers
+        {
+            get { return _numberOfPlayers; }
+            set
+            {
+                _numberOfPlayers = value;
+                OnPropertyChanged(nameof(NumberOfPlayers));
+            }
+        }
+        public int MinutesBeforeLoss { get; set; }
+        public ComputerSkillLevel ComputerSkill
+        {
+            get { return _computerSkill; }
+            set
+            {
+                _computerSkill = value;
+                OnPropertyChanged(nameof(ComputerSkill));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    [Serializable]
+    public enum ComputerSkillLevel
+    {
+        Dumb,
+        Good,
+        Cheats
+    }
+}
---
+++ karliCards Gui/Options.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace karliCards_Gui
+{
+    /// <summary>
+    /// Options.xaml 的交互逻辑
+    /// </summary>
+    public partial class Options : Window
+    {
+        private GameOptions _gameOptions;
+        public Options()
+        {
+            if (_gameOptions == null)
+            {
+                if (File.Exists("GameOptions.xml"))
+                {
+                    using (var stream = File.OpenRead("GameOptions.xml"))
+                    {
+                        //出现XML 文档(0, 0)中有错误。缺少根元素。在序列化之前将xml文档定位为0，（我直接删除GameOptions.xml文件）
+                        var serializer = new XmlSerializer(typeof(GameOptions));
+                        _gameOptions = serializer.Deserialize(stream) as GameOptions;
+                    }
+                }
+                else
+                    _gameOptions = new GameOptions();
+            }
+            DataContext = _gameOptions;
+            InitializeComponent();
+        }
+
+        private void numberOfPlayerComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void dumbAIRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            _gameOptions.ComputerSkill = ComputerSkillLevel.Dumb;
+        }
+
+        private void goodAIRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            _gameOptions.ComputerSkill = ComputerSkillLevel.Good;
+        }
+
+        private void cheatingAIRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            _gameOptions.ComputerSkill = ComputerSkillLevel.Cheats;
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            _gameOptions = null;
+            Close();
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var stream = File.Open("GameOptions.xml", FileMode.Create))
+            {
+                var serializer = new XmlSerializer(typeof(GameOptions));
+                serializer.Serialize(stream, _gameOptions);
+            }
+            Close();
+        }
+    }
+}
